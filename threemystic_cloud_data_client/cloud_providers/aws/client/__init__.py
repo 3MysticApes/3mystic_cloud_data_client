bc83fe99206d53b36dec689c48d807edf01029dc
Ron <tikicoder@gmail.com>
2023-07-15 20:58:32 -0700

    updates

--- threemystic_cloud_data_client/cloud_providers/aws/client/__init__.py
+++ threemystic_cloud_data_client/cloud_providers/aws/client/__init__.py
@@ -1,8 +1,9 @@
-from threemystic_cloud_cmdb.cloud_providers.base_class.base_client import cloud_cmdb_provider_base_client as base
+from threemystic_cloud_data_client.cloud_providers.base_class.base_client import cloud_data_client_provider_base_client as base
 
-
-class cloud_cmdb_aws_client(base):
+class cloud_data_client_aws_client(base):
   def __init__(self, *args, **kwargs):
-    super().__init__(provider= "aws", logger_name= "cloud_cmdb_aws_client", *args, **kwargs)
-
+    super().__init__(provider= "aws", logger_name= "cloud_data_client_aws_client", *args, **kwargs)
+    
+    
+  
   
