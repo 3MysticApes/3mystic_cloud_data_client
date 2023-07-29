ae362bbab10577cb3e3da26daaedb683e5964433
Ron <tikicoder@gmail.com>
2023-07-28 18:14:33 -0700

    updates for data query

--- threemystic_cloud_data_client/cloud_providers/aws/client/actions/budget.py
+++ threemystic_cloud_data_client/cloud_providers/aws/client/actions/budget.py
@@ -66,17 +66,17 @@ class cloud_data_client_aws_client_action(base):
       }
     }
   
-  async def __process_get_cost_data_process_year_data(self, year_data, client, account, start_date, end_date, fiscal_start, fiscal_end, forecast_metric = "NET_UNBLENDED_COST", *args, **kwargs):
+  async def __process_get_cost_data_process_year_data(self, year_data, client, account, start_date, end_date, fiscal_start, fiscal_end, forecast_metric = ["NET_UNBLENDED_COST"], *args, **kwargs):
 
     results_by_time = self.get_cloud_client().general_boto_call_array(
-      boto_call=lambda: client.get_cost_and_usage(
+      boto_call=lambda: client.get_cost_and_usage_with_resources(
         TimePeriod={
           'Start': start_date.strftime("%Y-%m-%d"),
           'End': end_date.strftime("%Y-%m-%d"),
         },
         Granularity='DAILY',
-        Metrics=[forecast_metric],
-        Filter={ 
+        Metrics=forecast_metric,
+        Filter={
           "Dimensions":{
             "Key":"LINKED_ACCOUNT",
             "Values":[self.get_cloud_client().get_account_id(account= account)]
